49a8988de6b8575da81a17880b26c87b7c29b068
Kaixinguo <kaixinguo@kaixinguo.site>
2019-01-01 02:45:57 +0800

    Add the Route of HeroDetailComponent

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,12 +9,8 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes: Hero[];
-  selectedHero: Hero;
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+  heroes: Hero[];
 
   getHeroes(): void {
     this.heroService.getHeroes()
